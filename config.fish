3e19ca0a5a1c1f8d5ee81bdac12d1f99b4154327
Stephan Mannhart <stephan.mannhart@googlemail.com>
2016-04-30 17:59:12 +0200

    Changed fish git prompt char for untracked files

--- config.fish
+++ config.fish
@@ -2,6 +2,7 @@ set -x LC_ALL en_US.UTF-8
 set -x LC_CTYPE en_US.UTF-8
 set -x PIP_REQUIRE_VIRTUALENV "true"
 eval (python -m virtualfish)
+set PATH /usr/local/sbin $PATH
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
@@ -15,7 +16,7 @@ set __fish_git_prompt_color_upstream_behind red
 # Status Chars
 set __fish_git_prompt_char_dirtystate '⚡ '
 set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_untrackedfiles '…'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '+'
 set __fish_git_prompt_char_upstream_behind '-'
